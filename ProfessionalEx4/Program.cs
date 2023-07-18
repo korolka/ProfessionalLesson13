bee708418f4be971df7912204511b7304f2089b2
korolka <korolka212@gmail.com>
2023-07-18 11:43:03 +0300

    new commit

---
+++ ProfessionalEx4/Program.cs
@@ -0,0 +1,27 @@
+﻿//Завдання 4
+
+//Створіть масив чисел розмірністю 1000000 або більше.
+//Використовуючи генератор випадкових чисел, проініціалізуйте цей масив значеннями.
+//Створіть PLINQ запит, який дозволить отримати усі непарні числа з вихідного масиву.
+
+namespace ProfessionalEx4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[] ints = new int[1000000];
+            Action<int> action = i => { ints[i] = random.Next(0, 1000); Console.WriteLine("Created numbres in array :"+ints[i]); };
+            Parallel.For(0, ints.Length, action);
+
+            ParallelQuery<int> query = from items in ints.AsParallel().OrderBy(i => i)
+                                       where items % 2 != 0
+                                       select items;
+            foreach (int i in query)
+            {
+                Console.WriteLine("Sorted result: " + i);
+            }
+        }
+    }
+}
---
+++ ProfessionalLesson13/Program.cs
@@ -0,0 +1,40 @@
+﻿//Завдання 2
+
+//Створіть два методи, які виконуватимуться у межах паралельних завдань.
+//Організуйте виклик цих методів за допомогою Invoke таким чином,
+//щоб основний потік програми (метод Main) не зупинив виконання.
+using System.Reflection;
+
+namespace ProfessionalLesson13
+{
+    internal class Program
+    {
+        static void Method1()
+        {
+            Console.WriteLine("{0} from task {1} from thread {2} started", MethodInfo.GetCurrentMethod().Name, Task.CurrentId, Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(2000);
+            Console.WriteLine("{0} from task {1} from thread {2} finished", MethodInfo.GetCurrentMethod().Name, Task.CurrentId, Thread.CurrentThread.ManagedThreadId);
+        }
+        static void Method2()
+        {
+            Console.WriteLine("{0} from task {1} from thread {2} started", MethodInfo.GetCurrentMethod().Name, Task.CurrentId, Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(2000);
+            Console.WriteLine("{0} from task {1} from thread {2} finished", MethodInfo.GetCurrentMethod().Name, Task.CurrentId, Thread.CurrentThread.ManagedThreadId);
+        }
+      
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start Main");
+            Thread instance1 = new Thread(Method1);
+            Thread instance2 = new Thread(Method2);
+            ParallelOptions paralel = new ParallelOptions();
+            paralel.MaxDegreeOfParallelism = 3;
+            Parallel.Invoke(paralel, instance1.Start, instance2.Start);
+            Thread.Sleep(1000);
+            Console.WriteLine("_________________________________-");
+            
+        }
+    }
+
+
+}
